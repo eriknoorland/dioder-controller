f228cce4fa95b02149b2ad854b33e269efd19292
Erik Noorland <mail@eriknoorland.nl>
2018-01-29 20:11:40 +0100

    Update colour on received MQTT message

--- dioderController/dioderController.ino
+++ dioderController/dioderController.ino
@@ -1,16 +1,20 @@
-#include <SPI.h>
 #include <UIPEthernet.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 // Ethernet settings
 byte mac[] = { 0x22, 0xCC, 0xDD, 0xEE, 0xEE, 0x01 };
-// IPAddress ip(192, 168, 1, 179);
 EthernetServer server(80);
 EthernetClient ethernetClient;
 
 // MQTT settings
 IPAddress broker(192,168,178,29);
 PubSubClient client(ethernetClient);
+
+// LED settings
+int PIN_RED = 9;
+int PIN_GREEN = 6;
+int PIN_BLUE = 3;
 
 /**
  * MQTT callback
@@ -23,19 +27,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print("] ");
 
-  for (int i = 0; i < length; i++) {
-    Serial.print(payload[i]);
-  }
-
-  // if (strcmp(topic, "light") == 0) {
-  //   if (payload[0] == '1') {
-  //     digitalWrite(OUTPUT_PIN_1, HIGH);
-  //     Serial.println("Relais on");
-  //   } else {
-  //     digitalWrite(OUTPUT_PIN_1, LOW);
-  //     Serial.println("Relais off");
-  //   }
+  // for (int i = 0; i < length; i++) {
+  //   Serial.print(payload[i]);
   // }
+
+  if (strcmp(topic, "dioder") == 0) {
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(payload);
+    JsonArray& colour = root["colour"];
+
+    int red = colour[0];
+    int green = colour[1];
+    int blue = colour[2];
+
+    analogWrite(PIN_RED, red);
+    analogWrite(PIN_GREEN, green);
+    analogWrite(PIN_BLUE, blue);
+  }
 
   Serial.println();
 }
@@ -64,6 +72,10 @@ void reconnect() {
  */
 void setup() {
   Serial.begin(9600);
+
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
 
   client.setServer(broker, 1881);
   client.setCallback(callback);
